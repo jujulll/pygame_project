821cb7f91b277e254204aa3c388d30409c59a443
jujulll <ivshinayv@gmail.com>
2022-01-31 17:32:12 +0500

    two levels

--- color_switch.py
+++ color_switch.py
@@ -3,8 +3,7 @@ import os
 import sys
 import random
 
-
-size = width, height = 500, 1000
+size = width, height = 300, 437
 pygame.init()
 pygame.display.set_caption("CoLoR SwItCh")
 
@@ -12,6 +11,11 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 fps = 15
 screen.fill((0, 0, 0))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 def load_image(name, colorkey=None):
@@ -23,13 +27,71 @@ def load_image(name, colorkey=None):
     return image
 
 
+def Start_screen():
+    size = width, height = 300, 437
+    background = load_image("screen_start.png")
+    screen.blit(background, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                Level_01()
+        pygame.display.flip()
+
+
+def load_level(name):
+    fullname = "data/" + name
+    with open(fullname, 'r') as map:
+        level_map = []
+        for line in map:
+            line = line.strip()
+            level_map.append(line)
+    return level_map
+
+
+def draw_level(level_map, a):
+    for y in range(len(level_map)):
+        for x in range(len(level_map[y])):
+            if level_map[y][x] == "#":
+                change = Change_color(y, a)
+                screen.blit(change.rot, change.rot_rect)
+            elif level_map[y][x] == "&":
+                circle = Circle(y, a)
+                screen.blit(circle.crcl_rot, circle.crcl_rot_rect)
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[1] * width for _ in range(height)]
+
+        self.x = 10
+        self.y = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.x = left
+        self.y = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(64, 64, 64), (
+                    x * self.cell_size + self.x, y * self.cell_size + self.y, self.cell_size, self.cell_size),
+                                 self.board[y][x])
+
+
 class Change_color(pygame.sprite.Sprite):
     def __init__(self, y, a):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image("change.png")
         self.img = pygame.transform.scale(self.image, (self.image.get_width() * 0.75, self.image.get_height() * 0.75))
         self.rot = pygame.transform.rotate(self.img, a)
-        self.rot_rect = self.rot.get_rect(centerx=248, centery=y * 2)
+        self.rot_rect = self.rot.get_rect(centerx=253, centery=y * 30 + 10)
 
 
 class Circle(pygame.sprite.Sprite):
@@ -37,7 +99,7 @@ class Circle(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.crcl = load_image("circle1.png")
         self.crcl_rot = pygame.transform.rotate(self.crcl, a)
-        self.crcl_rot_rect = self.crcl_rot.get_rect(centerx=248, centery=y)
+        self.crcl_rot_rect = self.crcl_rot.get_rect(centerx=252, centery=y * 30)
 
 
 class Start_Finish(pygame.sprite.Sprite):
@@ -47,39 +109,78 @@ class Start_Finish(pygame.sprite.Sprite):
         self.sf_r = self.sf.get_rect(center=(250, y))
 
 
-colors = [(195, 45, 255), (124, 255, 137), (0, 255, 242), (255, 255, 0)]
-c = (255, 255, 255)
-a = 20
-b = 10
-change1 = Change_color(960, a)
-start = Start_Finish(15)
-finish = Start_Finish(945)
-running = True
-flag = False
-y = 985
-while running:
-    screen.fill((0, 0, 0))
-    change1 = Change_color(400, a)
-    change2 = Change_color(300, a)
-    circle1 = Circle(600, b)
-    a += 3
-    b += 5
-    screen.blit(start.sf, start.sf_r)
-    screen.blit(finish.sf, finish.sf_r)
-    screen.blit(change1.rot, change1.rot_rect)
-    screen.blit(change2.rot, change2.rot_rect)
-    screen.blit(circle1.crcl_rot, circle1.crcl_rot_rect)
-    pygame.draw.circle(screen, (255, 255, 255), (250, y), 15)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                y -= 25
-            flag = True
-    if flag:
-        y += 3
-    
-    pygame.display.flip()
-    clock.tick(fps)
-pygame.quit()
+class Camera:
+    def __init__(self, y):
+        self.dx = 0
+        self.dy = 800
+
+    def update(self):
+        self.dy -= 400
+
+
+def game(name):
+    board = Board(16, 35)
+    board.set_view(0, 0, 30)
+    size = width, height = 480, 1020
+    screen = pygame.display.set_mode(size)
+    colors = [(195, 45, 255), (124, 255, 137), (0, 255, 242), (255, 255, 0)]
+    a = 20
+    f = 0
+    e = 0
+    z = 0
+    m = True
+    start = Start_Finish(15)
+    finish = Start_Finish(945)
+    running = True
+    flag = False
+    y = 985
+    while running:
+        screen.fill((0, 0, 0))
+        board.render(screen)
+        draw_level(load_level(name), a)
+
+        screen.blit(start.sf, start.sf_r)
+        screen.blit(finish.sf, finish.sf_r)
+        a += 3
+        if m:
+            pygame.draw.circle(screen, (255, 255, 255), (254, y), 15)
+        s = 0
+        if s == 3:
+            s -= 3
+        else:
+            s += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    y -= 25
+                flag = True
+        if flag:
+            y += 3
+        if f == 0:
+            e = colors[random.randint(0, 3)]
+            f += 1
+        if f == 1:
+            z = colors[random.randint(0, 3)]
+            f += 1
+        if 838 > y > 665:
+            pygame.draw.circle(screen, e, (254, y), 15)
+            m = False
+        if y < 665:
+            pygame.draw.circle(screen, z, (254, y), 15)
+        if y < 0:
+            return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def Level_01():
+    game('level1.txt')
+    Level_02()
+
+
+def Level_02():
+    game('level2.txt')
+
+Start_screen()
